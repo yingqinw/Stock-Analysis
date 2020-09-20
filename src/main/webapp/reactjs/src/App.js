b639c6f63eef961a933b306eda2a72667ba77ea0
Vivian Zhu <tongyu@usc.edu>
2020-09-20 19:57:20 +0800

    UI optimization with design matching

--- src/main/webapp/reactjs/src/App.js
+++ src/main/webapp/reactjs/src/App.js
@@ -2,73 +2,141 @@ import React from 'react';
 import {useState} from 'react';
 import './App.css';
 import styled from 'styled-components';
-import {ButtonGroup} from 'react-bootstrap';
 
 const Wrapper = styled.div`
-  width: 40%;
-  background-color: white;
-  padding: 2%;
-  box-shadow:5px 10px black;
+  font-family: 'Open Sans', sans-serif;
+  width: 400px;
+  height: 490px;
+  margin: auto;
+  padding: 80px;
+  position: relative;
+  border: 1px solid #e2e2e2;
+  background: #FFF;
+  z-index: 1;
+  box-shadow: -10px 10px 0;
+  box-sizing: border-box;
 `;
-const Row = styled.div`
-  font-family: 'Roboto', sans-serif;
-  color: black;
-  margin-top: 2%;
+const FormWrapper = styled.div`
+  margin-top: 10%;
 `;
 const Button = styled.button`
-  margin-top: 10%;
-  font-family: 'PT Sans', sans-serif;
-  background-color: black;
-  color: white;
-  font-weight: bold;
-  letter-spacing: 1.5px;
+  cursor: pointer;
+  background-color: #3e3e3e;
+  color: #FFF;
+  padding: 10px 25px 11px;
+  margin: 5px 15px 0 0;
+  height: 45px;
+  font-weight: 700;
   text-transform: uppercase;
-  border: none;
-  padding: 3% 5% 3% 5%;
-  font-size: 70%;
-  position: relative;
-  width: 40%;
+  letter-spacing: 0.2em;
+  transition: background-color .3s;
+  font-size: 0.8em;
+  margin-top: 10%;
+  &:hover, &:focus {
+    background-color: #b1b1b1;
+    > .arrow{
+      margin: 0px 0 4px 18px;
+    }
+  }
 `
 const Arrow = styled.div`
-  border: solid white;
-  border-width: 0 3px 3px 0;
+  position: relative;
   display: inline-block;
-  padding: 3px;
-  transform: rotate(-45deg);
-  -webkit-transform: rotate(-45deg);
-  position: absolute;
-  right: 10%;
-  top: calc(50% - 6px);
-  font-size: 80%;
-`
+  width: 20px;
+  vertical-align: middle;
+  color: #FFF;
+  margin: 0px 0 4px 8px;
+  transition: margin .3s;
+  &:before,&:after {
+    content: "";
+    border-radius: 2px;
+    position: absolute;
+  },
+  &:before {
+    right: 0;
+    top: 0;
+    width: 20px;
+    height: 2px;
+    transition: width .3s;
+    transform-origin: right top;
+    box-shadow: inset 0 0 0 32px;
+  }
+  &:after {
+    transform: rotate(45deg);
+    top: -5px;
+    right: 0;
+    width: 10px;
+    height: 10px;
+    border-width: 2px 2px 0 0;
+    border-style: solid;
+  }
+`;
+const Title = styled.div`
+  cursor: pointer;
+  padding: 0 40px 10px 0;
+  margin: 0 -4px 0 0;
+  height: 30px;
+  font-weight: 700;
+  text-transform: uppercase;
+  letter-spacing: 0.2em;
+  font-size: 0.8em;
+  color: #3e3e3e;
+  border-bottom: 5px solid #e2e2e2;
+  transition: color .2s, border-bottom .2s;
+  display: inline-block;
+  transition: color .2s, border-bottom .2s;
+`;
+const SelectedTitle = styled(Title)`
+  color: #3e3e3e;
+  border-bottom: 5px solid #3e3e3e;
+`;
 
 function App() {
-  const [login, setLogin] = useState(true);
+  const [selectLogin, setSelectLogin] = useState(true);
+  console.log(selectLogin);
   return (
-    <div className="App-header">
-        <Wrapper Wrapper>
-        <ButtonGroup toggle={true}>
-          <Button variant="secondary" onClick={()=>{setLogin(true)}}>Log in</Button>
-          <Button variant="secondary"onClick={()=>{setLogin(false)}}>Sign up</Button>
-        </ButtonGroup>
-        {
-          login ? <>
-          <Row>Username</Row>
-          <Row>Password</Row>
-          <Button>
-            login
-            <Arrow></Arrow>
-          </Button></> :
-          <><Row>Username</Row>
-          <Row>Password</Row>
-          <Row>Retype Password</Row>
-          <Row>Email</Row>
-          <Button>
-            Sign up
-            <Arrow></Arrow>
-          </Button></>
-        }
+    <div className="App">
+      <div className="App-header">
+        <Wrapper>
+          {
+            selectLogin ? 
+            <form id="login-form">
+              <div className="selectWrapper">
+                <SelectedTitle onClick={()=>{setSelectLogin(true)}}>Login</SelectedTitle>
+                <Title onClick={()=>{setSelectLogin(false)}}>Signup</Title>
+              </div>
+              <FormWrapper>
+                <div className="fields">
+                  <input type="text" placeholder="Username"/>
+                  <input type="password" placeholder="Password"/>
+                </div>
+                <Button>
+                  login
+                  <Arrow className="arrow"></Arrow>
+                </Button>
+              </FormWrapper>
+            </form> : 
+            <form id="signup-form">
+              <div className="selectWrapper">
+                <Title onClick={()=>{setSelectLogin(true)}}>Login</Title>
+                <SelectedTitle onClick={()=>{setSelectLogin(false)}}>Signup</SelectedTitle>
+              </div>
+              <FormWrapper>
+                <div className="fields">
+                  <input type="text" placeholder="Username"/>
+                  <input type="password" placeholder="Password"/>
+                  <input type="password" placeholder="Retype Password"/>
+                  <input type="email" placeholder="Email"/>
+                </div>
+                <Button>
+                  signup
+                  <Arrow className="arrow"></Arrow>
+                </Button>
+              </FormWrapper>
+            </form>
+          }
         </Wrapper>
+      </div>
     </div>
   );
 }
