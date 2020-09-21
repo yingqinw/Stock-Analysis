6cd7efe96a0964132a65aa019bb4e2e3b373cb43
Vivian Zhu <tongyu@usc.edu>
2020-09-21 17:46:49 +0800

    Add fetch function to frontend

--- src/main/webapp/reactjs/src/App.js
+++ src/main/webapp/reactjs/src/App.js
@@ -39,20 +39,23 @@ export default function() {
   }, [email]);
 
   const handleSubmit = (e) => {
-    e.preventDefault();
-    const alertMessage = [];
-    if(!validUserName) {
-      alertMessage.push("Username can only contain alphanumeric characters and should be longer than 5 characters.");
-    }
-    if(!validPass) {
-      alertMessage.push("Password should contain 1 uppercase, 1 lowercase and 1 numeric character and should be between 6-20 characters.");
-    }
-    if(!validEmail && !selectLogin) {
-      alertMessage.push("Email should have '@' and '.' characters.");
-    }
-    if(alertMessage.length !== 0) {
-      alert(alertMessage.join('\n'));
-    }
+    // e.preventDefault();
+    // const alertMessage = [];
+    // if(!validUserName) {
+    //   alertMessage.push("Username can only contain alphanumeric characters and should be longer than 5 characters.");
+    // }
+    // if(!validPass) {
+    //   alertMessage.push("Password should contain 1 uppercase, 1 lowercase and 1 numeric character and should be between 6-20 characters.");
+    // }
+    // if(!validEmail && !selectLogin) {
+    //   alertMessage.push("Email should have '@' and '.' characters.");
+    // }
+    // if(alertMessage.length !== 0) {
+    //   alert(alertMessage.join('\n'));
+    // }
+    fetch(`http://localhost:3306/Login?username=${username}&password=${password}&email=${email}`, {
+      method: 'POST'
+    });
   }
 
   return (
