bfa479164f21b856957c274593d17b512d050e39
KenZCen <zongyece@usc.edu>
2020-09-21 21:58:40 +0800

    get the API UI working

---
+++ src/test/java/csci310/servlets/DataServletTest.java
@@ -0,0 +1,14 @@
+package csci310.servlets;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DataServletTest {
+
+	@Test
+	public void testDoGet() {
+		assertTrue(true);
+	}
+
+}
