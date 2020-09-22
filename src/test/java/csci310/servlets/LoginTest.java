bb26da2a75d8eb520e147eda7880471f6e182da0
Hyunjae Cho <hyunjaec@usc.edu>
2020-09-23 01:49:28 +0900

    Update LoginTest

--- src/test/java/csci310/servlets/LoginTest.java
+++ src/test/java/csci310/servlets/LoginTest.java
@@ -19,13 +19,13 @@ public class LoginTest extends Mockito {
 	@Test
 	public void testDoPost() throws Exception {
 		
-		SQL.register("hyunjae","Hj1234");
+	SQL.register("hyunjae","Hj1234");
 		
-		HttpServletRequest request = mock(HttpServletRequest.class);       
+	HttpServletRequest request = mock(HttpServletRequest.class);       
         HttpServletResponse response = mock(HttpServletResponse.class);
         
         when(request.getParameter("username")).thenReturn("hyunjae");
-        when(request.getParameter("password")).thenReturn("Hj1234");
+        when(request.getParameter("password")).thenReturn("Hj1");
         
         StringWriter stringWriter = new StringWriter();
         PrintWriter writer = new PrintWriter(stringWriter);
@@ -35,22 +35,7 @@ public class LoginTest extends Mockito {
         
         writer.flush();
         
-        assertTrue(stringWriter.toString().contains("Wecolme, hyunjae"));
-        
-        HttpServletRequest request2 = mock(HttpServletRequest.class);       
-        HttpServletResponse response2 = mock(HttpServletResponse.class);
-        
-        when(request2.getParameter("username")).thenReturn("hyunjae");
-        when(request2.getParameter("password")).thenReturn("Hj1");
-        
-        StringWriter stringWriter2 = new StringWriter();
-        PrintWriter writer2 = new PrintWriter(stringWriter2);
-        when(response2.getWriter()).thenReturn(writer2);
-        
-        new Login().doPost(request2, response2);
-        
-        assertTrue(stringWriter2.toString().contains("Incorrect username or password. Please try again! :)"));
-		
+        assertTrue(stringWriter.toString().contains("Incorrect username or password. Please try again! :)"));	
 	}
 
 }
