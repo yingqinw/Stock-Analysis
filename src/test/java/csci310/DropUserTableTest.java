27f2fa5c7655fe03dd56373813b78de2dcc11f9d
Yingqin Wang <yingqinw@usc.edu>
2020-09-23 14:30:39 +0800

    Create Drop and Initialize database classes

---
+++ src/test/java/csci310/DropUserTableTest.java
@@ -0,0 +1,14 @@
+package csci310;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DropUserTableTest {
+
+	@Test
+	public void test() {
+		DropUserTable d = new DropUserTable();
+	}
+
+}
