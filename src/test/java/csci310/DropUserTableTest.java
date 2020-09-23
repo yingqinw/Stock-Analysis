dccfb14903736a6059988e339aed272cfd22f0c4
Hyunjae Cho <hyunjaec@usc.edu>
2020-09-23 16:18:17 +0900

    Add fix for drop user table testing

--- src/test/java/csci310/DropUserTableTest.java
+++ src/test/java/csci310/DropUserTableTest.java
@@ -8,7 +8,13 @@ public class DropUserTableTest {
 
 	@Test
 	public void test() {
+		SQL.register("nillamstrong","Firstman");
+		boolean user1 = SQL.userExist("nillamstrong");
+		assertTrue(user1);
+		
 		DropUserTable d = new DropUserTable();
+		boolean user2 = SQL.userExist("nillamstrong");
+		assertTrue(!user2);
 	}
 
 }
