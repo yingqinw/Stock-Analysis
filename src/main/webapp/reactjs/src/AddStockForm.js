901a21662b1c120ee91843450a0eb089c632df28
Vivian Zhu <tongyu@usc.edu>
2020-10-05 19:42:38 +0800

    Finish add stock pop up

--- src/main/webapp/reactjs/src/AddStockForm.js
+++ src/main/webapp/reactjs/src/AddStockForm.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import './App.css';
-import {FormWrapper, Button, Arrow} from './Modals';
+import {FormTitle, FormWrapper, Button, Arrow} from './Modals';
 
 
 export default function(props) {
   return (
     <form id="addStock-form" onSubmit={props.handleSubmit}>
-	  <FormWrapper>
+      <div className="selectWrapper">
+        <FormTitle>Add stock</FormTitle>
+        <i className="fa fa-times closeIcon" onClick={()=>{
+          props.setShowAddStockForm(false)
+        }}></i>
+      </div>
+      <FormWrapper>
         <div className="fields">
           <input type="text" placeholder="Ticker" onChange={(e) => {
             props.setTicker(e.target.value);
@@ -18,15 +24,15 @@ export default function(props) {
           }}
             style={{borderColor: props.validQuantity ? 'green':'red'}} 
           />
-		  <h1>Start Date</h1>
+          <h1>Start Date</h1>
           <input type="date" placeholder="start date"
             onChange={(e) => {
               props.setStartDate(e.target.value);
             }}
             style={{borderColor: props.validStart? 'green':'red'}} 
           />
-		  <h1>End Date (optional)</h1>
-		  <input type="date" placeholder="end date"
+          <h1>End Date (optional)</h1>
+          <input type="date" placeholder="end date"
             onChange={(e) => {
               props.setEndDate(e.target.value);
             }}
@@ -38,11 +44,9 @@ export default function(props) {
           <Arrow className="arrow"></Arrow>
         </Button>
       </FormWrapper>
-      
       <div className="alertWrapper">
-      	<p>{props.alertText}</p>
+        <p>{props.alertText}</p>
       </div>
-
 	</form>
   );
 }
