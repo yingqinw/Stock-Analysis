dd42a79b6051d23b0783ff3507f647e2a2484ad8
Hyunjae Cho <hyunjaec@usc.edu>
2020-10-14 13:40:53 +0900

    Minor Fix for title

--- src/main/webapp/reactjs/src/HomePage.js
+++ src/main/webapp/reactjs/src/HomePage.js
@@ -154,7 +154,7 @@ export default function(props) {
   return (
     <div className="homepageWrapper">
       <Navbar expand="lg" className="text-uppercase mb-3">
-        <Navbar.Brand className="nav_brand" style={{ color: '#f0f3fa' }} href="/">USC CS310 Stock Portfolio</Navbar.Brand>
+        <Navbar.Brand className="nav_brand" style={{ color: '#f0f3fa' }} href="/">USC CS310 Stock Portfolio Management</Navbar.Brand>
         <Navbar.Toggle className="justify-content-end" aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse className="justify-content-end" id="responsive-navbar-nav">
           <Navbar.Text>
