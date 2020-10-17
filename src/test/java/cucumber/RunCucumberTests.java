6a5f9852ba1602a8d6971f65e6278a371421d6fa
Hyunjae Cho <hyunjaec@usc.edu>
2020-10-18 02:34:49 +0900

    delete feature selection part

--- src/test/java/cucumber/RunCucumberTests.java
+++ src/test/java/cucumber/RunCucumberTests.java
@@ -10,7 +10,7 @@ import org.junit.runner.RunWith;
  * Run all the cucumber tests in the current package.
  */
 @RunWith(Cucumber.class)
-@CucumberOptions(strict = true, features = {"src/test/resources/cucumber/login.feature"})
+@CucumberOptions(strict = true)
 public class RunCucumberTests {
 
 	@BeforeClass
