980fc20c59df9ee282d166491a0482792d1ece10
KenZCen <zongyece@usc.edu>
2020-10-19 18:44:24 +0800

    done with implementation

---
+++ src/main/java/csci310/Portfolio.java
@@ -0,0 +1,130 @@
+
+
+package csci310;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.text.ParseException;
+import java.util.Scanner;
+import java.util.Vector;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import csci310.PriceArray;
+
+public class Portfolio {
+	String username;
+	Vector<PriceArray> stocks;
+	String PFstartDate;
+	String PFendDate;
+	
+	long PFstartDateEpoch;
+	long PFendDateEpoch;
+	Double[] portfolioValue;
+	String[] tradingDate;
+	private String APIKey = "btjeu1f48v6tfmo5erv0";
+	public Portfolio(String username, String startDate, String endDate) {
+		username = this.username;
+		this.PFstartDate = startDate;
+		this.PFendDate = endDate;
+		stocks = new Vector<PriceArray>();
+	}
+	public void addStock(String ticker, String StockStartDate, String StockEndDate) throws ParseException {
+		PriceArray PA = new PriceArray(ticker, StockStartDate, StockEndDate);
+		stocks.add(PA);
+	}
+	public void deleteStock(String ticker) {
+		boolean found = false;
+		for(int i=0; i<stocks.size(); i++) {
+			String temp = stocks.get(i).ticker;
+			if(temp.equals(ticker)) {
+				stocks.remove(i);
+				found = true;
+			}
+		}
+		if(found == false) System.out.println("Didn't find the to delete stock");
+	}
+	public void getPFTradingDate() throws IOException, ParseException {
+		//Assuming every stock on NYSE or nasdaq has the same trading date as IBM
+		PFstartDateEpoch = new java.text.SimpleDateFormat("MM/dd/yyyy HH:mm:ss").parse(PFstartDate +" 22:00:00").getTime() / 1000;
+		PFendDateEpoch = new java.text.SimpleDateFormat("MM/dd/yyyy HH:mm:ss").parse(PFendDate +" 22:00:00").getTime() / 1000;
+		
+		String website = "https://finnhub.io/api/v1/stock/candle?symbol="+ "IBM" +
+        		"&resolution=D&from=" + (int)(PFstartDateEpoch-86400) + 
+        		"&to=" + PFendDateEpoch + "&token=" + APIKey;
+        URL url = new URL(website);
+  		HttpURLConnection con = (HttpURLConnection) url.openConnection();
+  		con.setRequestMethod("GET");
+  		con.connect();
+		
+		int respondCode = con.getResponseCode(); 
+		if(respondCode != 200) throw new RuntimeException("HttpResponseCode:  "+ respondCode);
+		System.out.println("respond code: " + respondCode);
+
+  		//read json
+  		Scanner sc = new Scanner(url.openStream());
+  		String result = "";
+  		boolean hasContent = false;
+  		while(sc.hasNext()) {
+  			result += sc.nextLine();
+  			hasContent = true;
+  		}
+  		sc.close();
+  		//System.out.println(result);
+  		if(!hasContent) return;
+  		
+		//parse json 
+		JSONObject obj = new JSONObject(result);
+		if(obj.length() == 0) return;
+		JSONArray t = obj.getJSONArray("t");
+		int length = t.length();
+		tradingDate = new String[length];
+		portfolioValue = new Double[length];
+		for(int i=0; i<length; i++) {
+			long timeEpoch = t.getLong(i);
+			tradingDate[i] = new java.text.SimpleDateFormat("MM/dd/yyyy").format(new java.util.Date (timeEpoch*1000));
+		}
+	}
+	public void printPortfolio() {
+		System.out.println(">>>>Printing portfolio<<<<");
+		for(int z=0; z<tradingDate.length; z++) {
+			System.out.println("Date: "+ tradingDate[z] + " dayPFvalue: " + portfolioValue[z]);
+		}
+	}
+	public void populatePortfolioValue() throws IOException, ParseException {
+		getPFTradingDate(); 
+		//populate stock prices
+		for(int i=0; i<stocks.size(); i++) {
+			PriceArray temp = stocks.get(i);
+			temp.populateStockPrice();
+			//find the matching index
+			temp.syncIndex = -10000000;
+			for(int j=0; j<temp.tradingDate.length; j++) { //find PV start date in stock
+				if(temp.tradingDate[j].equals(PFstartDate)) temp.syncIndex = j; 
+			}
+			if(temp.syncIndex == -10000000){ //find stock start date in PV
+				for(int z=0; z<tradingDate.length; z++) {
+					if(tradingDate[z].equals(temp.startDate)) temp.syncIndex = -z;
+				}
+			}
+			//System.out.println(temp.ticker + "'s syncIndex: " + temp.syncIndex);
+		}
+		//loop thru everyday then every stock to calculate sum of stock prices
+		int PFDayCount = tradingDate.length;
+		for(int i= 0; i < PFDayCount; i++) { 
+			Double dayPFvalue = 0.0;
+			for(int j=0; j<stocks.size(); j++) {
+				int currSyncIndex = stocks.get(j).syncIndex;
+				//System.out.println("stock: " + stocks.get(j).ticker + " currSyncIndex: " + currSyncIndex + " stockprice length: " + stocks.get(j).stockPrice.length);
+				if(currSyncIndex >= 0 && currSyncIndex < stocks.get(j).stockPrice.length) {
+					dayPFvalue += stocks.get(j).stockPrice[currSyncIndex];
+					System.out.println("on date: " + tradingDate[i] + " day["+ i +"], adding day[" + (int)(currSyncIndex) + "]'s value of " + stocks.get(j).ticker + " to portfolio.");
+				}
+				stocks.get(j).syncIndex++; 
+			}
+			portfolioValue[i] = dayPFvalue;
+		}
+	}
+}
---
+++ src/main/java/csci310/PriceArray.java
@@ -0,0 +1,101 @@
+package csci310;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.text.ParseException;
+import java.util.Scanner;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class PriceArray {
+	public String ticker;
+	public String startDate;
+	public long startDateEpoch;
+	public long endDateEpoch;
+	public int syncIndex;
+	public String[] tradingDate;
+	public Double[] stockPrice;
+	private String APIKey = "btjeu1f48v6tfmo5erv0";
+	public PriceArray(String ticker, String startDate, String endDate) throws ParseException {
+		this.ticker = ticker;
+		this.startDate = startDate;
+		this.startDateEpoch = new java.text.SimpleDateFormat("MM/dd/yyyy HH:mm:ss").parse(startDate+" 22:00:00").getTime() / 1000;
+		this.endDateEpoch = new java.text.SimpleDateFormat("MM/dd/yyyy HH:mm:ss").parse(endDate+" 22:00:00").getTime() / 1000;
+		//this.endDateEpoch += 86400;
+	}
+	public void populateStockPrice() throws IOException, ParseException {
+		String website = "https://finnhub.io/api/v1/stock/candle?symbol="+ ticker +
+        		"&resolution=D&from=" + (int)(startDateEpoch-86400) + 
+        		"&to=" + (int)(endDateEpoch) + "&token=" + APIKey;
+        URL url = new URL(website);
+  		HttpURLConnection con = (HttpURLConnection) url.openConnection();
+  		con.setRequestMethod("GET");
+  		con.connect();
+		
+		int respondCode = con.getResponseCode(); 
+		if(respondCode != 200) throw new RuntimeException("HttpResponseCode:  "+ respondCode);
+		System.out.println("respond code: " + respondCode);
+
+  		//read json
+  		Scanner sc = new Scanner(url.openStream());
+  		String result = "";
+  		boolean hasContent = false;
+  		while(sc.hasNext()) {
+  			result += sc.nextLine();
+  			hasContent = true;
+  		}
+  		sc.close();
+  		//System.out.println(result);
+  		
+		//parse json 
+		JSONObject obj = new JSONObject(result);
+		if(obj.length() == 0) return;
+		JSONArray c = obj.getJSONArray("c");
+		JSONArray t = obj.getJSONArray("t");
+		int length = c.length();
+		stockPrice = new Double[length];
+		tradingDate = new String[length];
+		for(int i=0; i<length; i++) {
+			stockPrice[i] = c.getDouble(i);
+			long timeEpoch = t.getLong(i);
+			tradingDate[i] = new java.text.SimpleDateFormat("MM/dd/yyyy").format(new java.util.Date (timeEpoch*1000));
+		}
+		predictFuturePrices();
+	}
+	public void printPriceArray() {
+		for(int i=0; i<tradingDate.length; i++) {
+			System.out.println("Time: " + tradingDate[i] + " Price: " + stockPrice[i]);
+		}
+	}
+	public void predictFuturePrices() throws ParseException{
+		int historialDayCount = tradingDate.length;
+		String currentDate = tradingDate[historialDayCount-1];
+		long currentDateEpoch = new java.text.SimpleDateFormat("MM/dd/yyyy HH:mm:ss").parse(currentDate+" 22:00:00").getTime() / 1000;
+		int futureDayCount = (int) ((endDateEpoch - currentDateEpoch)/86400);
+		if(futureDayCount <= 0) return;
+		
+		String[] tradingDate2 = new String[historialDayCount + futureDayCount];
+		Double[] stockPrice2 = new Double[historialDayCount + futureDayCount];
+		//copy historial to new array
+		for(int i=0; i<historialDayCount; i++) {
+			tradingDate2[i] = tradingDate[i];
+			stockPrice2[i] = stockPrice[i];
+		} 
+		//predict future prices with 5-MA
+		for(int i=0; i<futureDayCount; i++) {
+			Double currPrice = 0.0;
+			int MovingAvgDays = 5;
+			for(int j=0; j<MovingAvgDays; j++) {
+				currPrice += stockPrice2[historialDayCount + i - MovingAvgDays + j];
+			}
+			long timeEpoch = currentDateEpoch + 86400*(i+1);
+			tradingDate2[historialDayCount + i] = new java.text.SimpleDateFormat("MM/dd/yyyy").format(new java.util.Date (timeEpoch*1000));
+			stockPrice2[historialDayCount + i] = currPrice/MovingAvgDays;
+		}
+		tradingDate = tradingDate2;
+		stockPrice = stockPrice2;
+	}
+}
+
