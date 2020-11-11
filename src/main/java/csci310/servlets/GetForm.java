1f6f80457b3594525375444a2055906ceeca0172
Yingqin Wang <yingqinw@usc.edu>
2020-11-11 18:38:16 +0800

    update

--- src/main/java/csci310/servlets/GetForm.java
+++ src/main/java/csci310/servlets/GetForm.java
@@ -336,7 +336,7 @@ public class GetForm extends HttpServlet {
         		  				price.put(p.portfolioValue[j]);
         		  				date.put(p.tradingDate[j]);
         		  			}
-        		  			AddStockData asd = new AddStockData(date,price,updatedPrices,p.getCurrPortfolioValue(),(int)(p.getCurrPortfolioValue()/p.getPrevPortfolioValue())-100);
+        		  			AddStockData asd = new AddStockData(date,price,updatedPrices,p.getCurrPortfolioValue(),(int)(p.getCurrPortfolioValue()*100/p.getPrevPortfolioValue())-100);
         		  		    response.setContentType("application/json");
         		  		    response.setCharacterEncoding("UTF-8");
         		  		    out.print(this.gson.toJson(asd));
