1b0047b9265fc85b3a9dff0ab9dca91c4faf1326
Yingqin Wang <yingqinw@usc.edu>
2020-11-12 01:27:50 +0800

    Update AddStock.java

--- src/main/java/csci310/servlets/AddStock.java
+++ src/main/java/csci310/servlets/AddStock.java
@@ -203,9 +203,7 @@ public class AddStock extends HttpServlet {
 		  				price.put(p.portfolioValue[j]);
 		  				date.put(p.tradingDate[j]);
 		  			}
-					AddStockData asd = new AddStockData(date,price,updatedPrices,0,0);
-					if(p.getCurrPortfolioValue()!=0)
-		  			asd = new AddStockData(date,price,updatedPrices,p.getCurrPortfolioValue(),(int)(p.getCurrPortfolioValue()*100/p.getPrevPortfolioValue())-100);
+					AddStockData asd = new AddStockData(date,price,updatedPrices,p.getCurrPortfolioValue(),(p.getCurrPortfolioValue()=0)?0:(int)(p.getCurrPortfolioValue()*100/p.getPrevPortfolioValue())-100);
 					
 		  		    response.setContentType("application/json");
 		  		    response.setCharacterEncoding("UTF-8");
