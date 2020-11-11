92eef99a60f73a2be65fa1db826c76c4efe22996
Yingqin Wang <yingqinw@usc.edu>
2020-11-12 01:53:02 +0800

    Update UpdatePortfolio.java

--- src/main/java/csci310/servlets/UpdatePortfolio.java
+++ src/main/java/csci310/servlets/UpdatePortfolio.java
@@ -92,9 +92,8 @@ public class UpdatePortfolio extends HttpServlet{
 			for(int i =0;i<p.tradingDate.length;i++) {
 				price.put(p.portfolioValue[i]);
 			}
-			AddStockData asd = new AddStockData(price,0,0);
-			if(p.getCurrPortfolioValue()!=0)
-			asd = new AddStockData(price,p.getCurrPortfolioValue(),(p.getCurrPortfolioValue()==0)?0: (int)(p.getCurrPortfolioValue()*100/p.getPrevPortfolioValue())-100);
+			double temp =p.getCurrPortfolioValue();
+			AddStockData asd = new AddStockData(price,temp,(temp==0)?0: (int)(temp*100/p.getPrevPortfolioValue())-100);
 		    response.setContentType("application/json");
 		    response.setCharacterEncoding("UTF-8");
 		    out.print(this.gson.toJson(asd));
